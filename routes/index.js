8612547729aa0760dfe8cb73cd1fc08e964bf662
panyawen <alice850101@gmail.com>
2018-12-24 20:07:06 +0800

    add

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ 
+    // res.render('index', { title: 'hello' });
+    var list={
+        1:{n:'sss'},
+        2:{n:'aaa'}
+    };
+
+    res.render('index', { 
+        title: 'hello', 
+        myName : '洧杰',
+        list: list,
+        tag: "<h1>hi</h1>" }
+        );
+});
+
+module.exports = router;
