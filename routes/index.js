821dcf9c63fa936d046180cce33d146f403c27eb
panyawen <alice850101@gmail.com>
2019-01-07 11:30:00 +0800

    新增註冊登入功能

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+// Initialize Firebase
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  
@@ -8,6 +10,7 @@ router.get('/', function(req, res, next) {
         1:{n:'sss'},
         2:{n:'aaa'}
     };
+
 
     res.render('index', { 
         title: 'hello', 
--- routes/member.js
+++ routes/member.js
@@ -4,12 +4,22 @@ var admin = require("firebase-admin");
 
 /* GET home page. */
 router.get('/profile', function (req, res, next) {
-    res.render('profile', { title: '33',data :'這是會員頁面' });
+    res.render('profile', { title: '會員資料',data :'這是會員頁面' });
 
 });
 
-router.get('/img', function (req, res, next) {
-    res.render('img', { title: '33',data :'這是img頁面' });
+router.get('/login', function (req, res, next) {
+    res.render('login', { title: '登入',data :'' });
+
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('register', { title: '註冊',data :'' });
+
+});
+
+router.get('/cart', function (req, res, next) {
+    res.render('cart', { title: '購物車',data :'這購物車頁面' });
 
 });
 
